d3a763fe9ba65e40929025ff21b9a937ff40887c
sijoon-sung <162120398+sijoon-sung@users.noreply.github.com>
2024-07-22 10:24:29 +0900

    Add files via upload

---
+++ Queue/QueueExample.java
@@ -0,0 +1,31 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Message> messageQueue = new LinkedList<Message>();
+
+        messageQueue.offer(new Message("sendMail", "홍길동"));
+        messageQueue.offer(new Message("sendSMS", "신용권"));
+        messageQueue.offer(new Message("sendKakao", "홍두께"));
+
+        while (!messageQueue.isEmpty()){// 메세지 큐가 비었는지 확인
+            Message message =messageQueue.poll(); // 메세지 큐에서 1개의 메세지를 꺼냄
+            switch (message.command){
+                case "sendMail":
+                    System.out.println(message.to+ "님에게 메일을 보냅니다");
+                    break;
+                case "sendSMS":
+                    System.out.println(message.to+ "님에게 SMS를 보냅니다");
+                    break;
+                case "sendKakao":
+                    System.out.println(message.to+ "님에게 카카오톡을 보냅니다");
+                    break;
+
+            }
+
+        }
+    }
+}
